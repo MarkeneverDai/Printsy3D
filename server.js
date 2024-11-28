35d396deaca53260efbf23ba96c05c090d6b5043
MarkeneverDai <markenever.d@gmail.com>
2024-11-27 16:29:54 -1000

    Co-authored-by: MikeHi1 <MikeHi1@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
 const cors = require('cors');
